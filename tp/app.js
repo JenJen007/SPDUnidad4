5f43783f654b61de9bda4a09a5a9983f2eff7074
lauberna <laureanoberna2002@gmail.com>
2023-10-18 20:31:59 -0300

    laureano

---
+++ tp/app.js
@@ -0,0 +1,9 @@
+const conclusion = document.querySelector(".contenedor_icon");
+const parrafos = document.querySelector(".conteneder_parrafos");
+
+conclusion.addEventListener("click", () => {
+  parrafos.classList.toggle("mostrar_parrafos");
+});
+
+
+
